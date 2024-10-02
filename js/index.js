248d3e7072b760772c31a0928cd5a406d8cc7571
llnikoll <niko.oviedo@gmail.com>
2024-10-01 21:25:27 -0400

    Cambios de Hamburgesa

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
+// Obtener el elemento del menú hamburguesa
+var hamburgerMenu = document.getElementById('hamburger-menu');
 
-// Función para alternar el menú de navegación en pantallas móviles
-document.getElementById('hamburger-menu').addEventListener('click', function() {
-    var navLinks = document.getElementById('nav-links');
+// Obtener el contenedor de los enlaces de navegación
+var navLinks = document.getElementById('nav-links');
+
+// Agregar un event listener al menú hamburguesa
+hamburgerMenu.addEventListener('click', function() {
+    // Alternar la clase 'active' en el contenedor de enlaces de navegación
     navLinks.classList.toggle('active');
 });
-
 
