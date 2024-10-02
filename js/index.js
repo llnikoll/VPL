adac0bc0aa624206c769fc1c954770c2bb5b47ce
llnikoll <niko.oviedo@gmail.com>
2024-10-01 20:56:21 -0400

    Cambio completo
    Cambio Completo

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 
+// Función para alternar el menú de navegación en pantallas móviles
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    var navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
 
 
