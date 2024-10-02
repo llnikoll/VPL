5879da127ff21d9e8c27bfee41b2a11996035d33
llnikoll <niko.oviedo@gmail.com>
2024-10-01 21:28:51 -0400

    cambios en clickear

--- js/index.js
+++ js/index.js
@@ -10,3 +10,30 @@ hamburgerMenu.addEventListener('click', function() {
     navLinks.classList.toggle('active');
 });
 
+// Funcionalidad para desplegar/ocultar los dropdowns
+var dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('click', function(event) {
+        // Evitar que el clic en el texto cierre el menú
+        event.stopPropagation();
+
+        // Alternar la clase 'active' en el contenido del dropdown
+        var dropdownContent = this.querySelector('.dropdown-content');
+        dropdownContent.classList.toggle('active');
+        
+        // Cerrar otros dropdowns si están abiertos
+        dropdowns.forEach(function(otherDropdown) {
+            if (otherDropdown !== dropdown) {
+                otherDropdown.querySelector('.dropdown-content').classList.remove('active');
+            }
+        });
+    });
+});
+
+// Cerrar dropdowns al hacer clic fuera de ellos
+document.addEventListener('click', function() {
+    dropdowns.forEach(function(dropdown) {
+        dropdown.querySelector('.dropdown-content').classList.remove('active');
+    });
+});
